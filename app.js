7a2ee41955e726f2fb8764d0c31b97a990a63932
Mohamedkamel10 <111062223+Mohamedkamel10@users.noreply.github.com>
2025-12-19 23:59:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-// Get path parts
 const parts = window.location.pathname.split("/").filter(Boolean);
-
-// لو شغال على GitHub Pages
-// parts = ["profile-system", "mohamed"]
-// آخر عنصر هو اسم اليوزر
 const username = parts.length > 1 ? parts[parts.length - 1] : "mohamed";
 
 fetch(`data/${username}.json`)
-  .then(res => {
-    if (!res.ok) throw new Error("User not found");
-    return res.json();
-  })
+  .then(res => res.json())
   .then(data => {
     document.getElementById("name").innerText = data.name;
     document.getElementById("job").innerText = data.job;
     document.getElementById("place").innerText = data.place;
 
-    document.querySelector(".profile-pic").style.backgroundImage =
-      `url(${data.avatar})`;
+    document.getElementById("avatar").src = data.avatar;
+    document.getElementById("cover").style.backgroundImage = `url(${data.cover})`;
 
-    document.querySelector(".header-img").style.backgroundImage =
-      `url(${data.cover})`;
+    document.getElementById("phone").href = `tel:${data.phone}`;
+    document.getElementById("email").href = `mailto:${data.email}`;
+
+    document.getElementById("whatsapp").href = data.socials.whatsapp;
+    document.getElementById("facebook").href = data.socials.facebook;
+    document.getElementById("instagram").href = data.socials.instagram;
+    document.getElementById("tiktok").href = data.socials.tiktok;
   })
   .catch(() => {
     document.body.innerHTML = "<h2 style='text-align:center'>User not found</h2>";
